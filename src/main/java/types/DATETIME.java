a840e690eea911f404d7a65c8e50df0414a60ecd
Nick Dimiduk <ndimiduk@gmail.com>
2013-03-25 17:22:18 -0700

    Implement DATETIME.
    DATETIME delegates to LONG.

--- src/main/java/types/DATETIME.java
+++ src/main/java/types/DATETIME.java
@@ -1,29 +1,69 @@
 package types;
 
-
 import java.nio.ByteBuffer;
 import java.util.Date;
 
 import util.HSerializer;
 
+/**
+ * Serializer for HBase DATETIME values.
+ * <p>
+ * The HBase DATETIME represents an instant in time. It is stored as a
+ * {@link LONG} value representing milliseconds from the epoch.
+ * </p>
+ */
 public class DATETIME extends HSerializer<Date> {
+
+  static final int SIZEOF_DATETIME = LONG.SIZEOF_LONG;
 
   @Override
   public byte[] toBytes(Date val) {
-    // TODO Auto-generated method stub
-    return null;
+    return toBytes(val.getTime(), order);
   }
 
   @Override
   public void putBytes(ByteBuffer buff, Date val) {
-    // TODO Auto-generated method stub
-
+    putBytes(buff, val.getTime(), order);
   }
 
   @Override
   public Date fromBytes(byte[] bytes) {
-    // TODO Auto-generated method stub
-    return null;
+    return new Date(LONG.toLong(bytes, 0, order));
   }
 
+  //
+  // Helper methods to mimic {@link Bytes}
+  //
+
+  public static long toLong(final byte[] bytes) {
+    return toLong(bytes, 0, DEFAULT_ORDER);
+  }
+
+  public static long toLong(final byte[] bytes, final Order order) {
+    return toLong(bytes, 0, order);
+  }
+
+  public static long toLong(final byte[] bytes, final int offset) {
+    return toLong(bytes, offset, DEFAULT_ORDER);
+  }
+
+  public static long toLong(final byte[] bytes, final int offset, Order order) {
+    return LONG.toLong(bytes, offset, order);
+  }
+
+  public static byte[] toBytes(long val) {
+    return toBytes(val, DEFAULT_ORDER);
+  }
+
+  public static byte[] toBytes(long val, Order order) {
+    return LONG.toBytes(val, order);
+  }
+
+  public static void putBytes(final ByteBuffer buff, long val) {
+    putBytes(buff, val, DEFAULT_ORDER);
+  }
+
+  public static void putBytes(final ByteBuffer buff, long val, Order order) {
+    LONG.putBytes(buff, val, order);
+  }
 }
