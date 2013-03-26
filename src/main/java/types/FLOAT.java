56dde2d10788f89e18608cef8bc0ace13357d3f0
Nick Dimiduk <ndimiduk@gmail.com>
2013-03-25 17:21:54 -0700

    Implement INT, FLOAT
    modeled after LONG, DOUBLE.

--- src/main/java/types/FLOAT.java
+++ src/main/java/types/FLOAT.java
@@ -1,28 +1,109 @@
 package types;
 
+import static types.INT.getRaw;
+import static types.INT.putRaw;
 
 import java.nio.ByteBuffer;
 
 import util.HSerializer;
 
+/**
+ * Serializer for HBase FLOAT values.
+ * <p>
+ * The HBase FLOAT is a 4-byte decimal number of varying precision, as defined
+ * by the IEEE 754 standard.
+ * </p>
+ * <p>
+ * The number is first interpreted as a whole number. Then, the sign bit is
+ * inverted and also invert the exponent and significand bits if the original
+ * value was negative. The resulting value bytes are stored in Big-Endian
+ * order. When sort order is specified as DESCENDING, the encoded value is
+ * logically inverted.
+ * </p>
+ */
 public class FLOAT extends HSerializer<Float> {
+
+  static final int SIZEOF_FLOAT = INT.SIZEOF_INT;
 
   @Override
   public byte[] toBytes(Float val) {
-    // TODO Auto-generated method stub
-    return null;
+    return toBytes(val, order);
   }
 
   @Override
   public void putBytes(ByteBuffer buff, Float val) {
-    // TODO Auto-generated method stub
-
+    putBytes(buff, val, order);
   }
 
   @Override
   public Float fromBytes(byte[] bytes) {
-    // TODO Auto-generated method stub
-    return null;
+    return toFloat(bytes, 0, order);
   }
 
+  //
+  // Helper methods to mimic {@link Bytes}
+  //
+
+  public static float toFloat(final byte[] bytes) {
+    return toFloat(bytes, 0, DEFAULT_ORDER);
+  }
+
+  public static float toFloat(final byte[] bytes, final Order order) {
+    return toFloat(bytes, 0, order);
+  }
+
+  public static float toFloat(final byte[] bytes, final int offset) {
+    return toFloat(bytes, offset, DEFAULT_ORDER);
+  }
+
+  public static float toFloat(final byte[] bytes, final int offset, Order order) {
+    return fromRawInt(getRaw(bytes, offset), order);
+  }
+
+  public static byte[] toBytes(float val) {
+    return toBytes(val, DEFAULT_ORDER);
+  }
+
+  public static byte[] toBytes(float val, Order order) {
+    byte[] buff = new byte[SIZEOF_FLOAT];
+    putRaw(buff, 0, toRawInt(val, order));
+    return buff;
+  }
+
+  public static void putBytes(final ByteBuffer buff, float val) {
+    putBytes(buff, val, DEFAULT_ORDER);
+  }
+
+  public static void putBytes(final ByteBuffer buff, float val, Order order) {
+    putRaw(buff, toRawInt(val, order));
+  }
+
+  //
+  // Helper methods for interoping between float values and 4 unsigned byte
+  // sequences stored in an int.
+  //
+
+  /**
+   * Convert a float containing an application-layer value into a <i>raw
+   * int</i>, a sequence of 4 unsigned bytes. "Raw" in this context is
+   * <b>unrelated</b> to "raw" from {@link Float#floatToRawIntBits(float)}.
+   * @param val The value to serialize.
+   * @param order the <code>Order</code> direction to respect.
+   * @return 4 unsigned bytes stored in an <code>int</code>.
+   */
+  protected static int toRawInt(float val, Order order) {
+    int raw = Float.floatToIntBits(val);
+    return (raw ^ ((raw >> Integer.SIZE - 1) | Integer.MIN_VALUE));
+  }
+
+  /**
+   * Convert a <i>raw int</i> containing a sequence of 4 unsigned bytes into a
+   * float application-layer value.
+   * @param raw the 4 unsigned bytes to deserialize.
+   * @param order the <code>Order</code> direction to respect.
+   * @return an application value.
+   */
+  protected static float fromRawInt(int raw, Order order) {
+    return Float.intBitsToFloat((~raw >> Integer.SIZE - 1) | Integer.MIN_VALUE);
+  }
 }
--- src/main/java/types/INT.java
+++ src/main/java/types/INT.java
@@ -1,28 +1,158 @@
 package types;
 
-
 import java.nio.ByteBuffer;
 
 import util.HSerializer;
 
+/**
+ * Serializer for HBase INT values.
+ * <p>
+ * The HBase LONG is an 4-byte, signed, two's complement integer stored in
+ * Big-Endian byte order.
+ * </p>
+ * <p>
+ * Serialization is performed by inverting the <code>int</code> sign bit and
+ * writing the value to a byte array in big endian order. For DESCENDING sort
+ * order, the encoded value is logically inverted.
+ * </p>
+ */
 public class INT extends HSerializer<Integer> {
+
+  static final int SIZEOF_INT = Integer.SIZE / Byte.SIZE;
 
   @Override
   public byte[] toBytes(Integer val) {
-    // TODO Auto-generated method stub
-    return null;
+    return toBytes(val, order);
   }
 
   @Override
   public void putBytes(ByteBuffer buff, Integer val) {
-    // TODO Auto-generated method stub
-
+    putBytes(buff, val, order);
   }
 
   @Override
   public Integer fromBytes(byte[] bytes) {
-    // TODO Auto-generated method stub
-    return null;
+    // TODO(perf): should we manage our own Integer instance cache?
+    return Integer.valueOf(toInt(bytes, 0, order));
   }
 
+  //
+  // Helper methods to mimic {@link Bytes}
+  //
+
+  public static int toInt(final byte[] bytes) {
+    return toInt(bytes, 0, DEFAULT_ORDER);
+  }
+
+  public static int toInt(final byte[] bytes, final Order order) {
+    return toInt(bytes, 0, order);
+  }
+
+  public static int toInt(final byte[] bytes, final int offset) {
+    return toInt(bytes, offset, DEFAULT_ORDER);
+  }
+
+  public static int toInt(final byte[] bytes, final int offset, Order order) {
+    return fromRawInt(getRaw(bytes, offset), order);
+  }
+
+  public static byte[] toBytes(int val) {
+    return toBytes(val, DEFAULT_ORDER);
+  }
+
+  public static byte[] toBytes(int val, Order order) {
+    byte[] buff = new byte[SIZEOF_INT];
+    putRaw(buff, 0, toRawInt(val, order));
+    return buff;
+  }
+
+  public static void putBytes(final ByteBuffer buff, int val) {
+    putBytes(buff, val, DEFAULT_ORDER);
+  }
+
+  public static void putBytes(final ByteBuffer buff, int val, Order order) {
+    putRaw(buff, toRawInt(val, order));
+  }
+
+  //
+  // Helper methods for interoping between int values and 4 unsigned byte
+  // sequences stored in an int.
+  //
+
+  /**
+   * Convert a <i>value int</i> containing an application-layer value into a
+   * <i>raw int</i>, a sequence of 4 unsigned bytes.
+   * @param val The value to serialize
+   * @param order The <code>Order</code> direction to respect.
+   * @return 4 unsigned bytes stored in an <code>int</code>.
+   */
+  protected static int toRawInt(int val, Order order) {
+    return val ^ Integer.MIN_VALUE ^ order.mask;
+  }
+
+  /**
+   * Convert a <i>raw int<i> containing a sequence of 4 unsigned bytes into a
+   * <i>value int</i>, an application-layer value.
+   * @param raw The 4 unsigned bytes to deserialize.
+   * @param order The <code>Order</code> direction to respect.
+   * @return an application value.
+   */
+  protected static int fromRawInt(int raw, Order order) {
+    return raw ^ Integer.MIN_VALUE ^ order.mask;
+  }
+
+  //
+  // Helper methods for reading/writing 4 unsigned byte sequences from/to
+  // byte[]'s and ByteBuffer's.
+  //
+
+  /**
+   * Put a sequence of 4 unsigned bytes as <code>raw</code> into
+   * <code>buff</code>.
+   * @param buff The buffer to receive the bytes.
+   * @param raw 4 unsigned bytes.
+   */
+  protected static void putRaw(ByteBuffer buff, int raw) {
+    putRaw(buff.array(), buff.arrayOffset() + buff.position(), raw);
+    buff.position(buff.position() + SIZEOF_INT);
+  }
+
+  /**
+   * Put a sequence of 4 unsigned bytes as <code>raw</code> into <code>buff</code>.
+   * @param buff The buffer to receive the bytes.
+   * @param raw 4 unsigned bytes.
+   */
+  protected static void putRaw(byte[] buff, int offset, int raw) {
+    for (int i = offset + SIZEOF_INT - 1; i >= offset; i--) {
+      buff[i] = (byte) raw;
+      raw >>>= 8;
+    }
+  }
+
+  /**
+   * Read a sequence of 4 unsigned bytes as a <code>raw</code> from <code>buff</code>.
+   * @param buff The buffer from which to retrieve bytes.
+   * @return 4 unsigned bytes in a <code>raw long</code>.
+   */
+  protected static int getRaw(ByteBuffer buff) {
+    int raw = getRaw(buff.array(), buff.arrayOffset() + buff.position());
+    buff.position(buff.position() + SIZEOF_INT);
+    return raw;
+  }
+
+  /**
+   * Read a sequence of 4 unsigned bytes as a <code>raw</code> from <code>buff</code>.
+   * @param buff The buffer from which to retrieve bytes.
+   * @param offset position in buff from which to start reading.
+   * @return 4 unsigned bytes in a <code>raw long</code>.
+   */
+  protected static int getRaw(byte[] buff, int offset) {
+    assert buff.length >= offset + SIZEOF_INT;
+    int j = 0;
+    for (int i = offset; i < offset + SIZEOF_INT; i++) {
+      j <<= 8;
+      j ^= buff[i] & 0xFF;
+    }
+    return j;
+  }
 }
