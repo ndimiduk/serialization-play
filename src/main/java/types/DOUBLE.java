453d77f89681c3557606efdeb732919e465c9e89
Nick Dimiduk <ndimiduk@gmail.com>
2013-03-25 17:22:53 -0700

    Refactor serialization strategy
    LONG serialization is made into a composition of serializing an user
    value to a raw representation and writing the raw representation to a
    buffer.

--- src/main/java/types/DOUBLE.java
+++ src/main/java/types/DOUBLE.java
@@ -1,28 +1,109 @@
 package types;
 
+import static types.LONG.getRaw;
+import static types.LONG.putRaw;
 
 import java.nio.ByteBuffer;
 
 import util.HSerializer;
 
+/**
+ * Serializer for HBase DOUBLE values.
+ * <p>
+ * The HBase DOUBLE is an 8-byte decimal number of varying precision, as
+ * defined by the IEEE 754 standard.
+ * </p>
+ * <p>
+ * The number is first interpreted as a whole number. Then, the sign bit is
+ * inverted and also invert the exponent and significand bits if the original
+ * value was negative. The resulting value bytes are stored in Big-Endian
+ * order. When sort order is specified as DESCENDING, the encoded value is
+ * logically inverted.
+ * </p>
+ */
 public class DOUBLE extends HSerializer<Double> {
+
+  static final int SIZEOF_DOUBLE = LONG.SIZEOF_LONG;
 
   @Override
   public byte[] toBytes(Double val) {
-    // TODO Auto-generated method stub
-    return null;
+    return toBytes(val, order);
   }
 
   @Override
   public void putBytes(ByteBuffer buff, Double val) {
-    // TODO Auto-generated method stub
-
+    putBytes(buff, val, order);
   }
 
   @Override
   public Double fromBytes(byte[] bytes) {
-    // TODO Auto-generated method stub
-    return null;
+    return toDouble(bytes, 0, order);
   }
 
+  //
+  // Helper methods to mimic {@link Bytes}
+  //
+
+  public static double toDouble(final byte[] bytes) {
+    return toDouble(bytes, 0, DEFAULT_ORDER);
+  }
+
+  public static double toDouble(final byte[] bytes, final Order order) {
+    return toDouble(bytes, 0, order);
+  }
+
+  public static double toDouble(final byte[] bytes, final int offset) {
+    return toDouble(bytes, offset, DEFAULT_ORDER);
+  }
+
+  public static double toDouble(final byte[] bytes, final int offset, Order order) {
+    return fromRawLong(getRaw(bytes, offset), order);
+  }
+
+  public static byte[] toBytes(double val) {
+    return toBytes(val, DEFAULT_ORDER);
+  }
+
+  public static byte[] toBytes(double val, Order order) {
+    byte[] buff = new byte[SIZEOF_DOUBLE];
+    putRaw(buff, 0, toRawLong(val, order));
+    return buff;
+  }
+
+  public static void putBytes(final ByteBuffer buff, double val) {
+    putBytes(buff, val, DEFAULT_ORDER);
+  }
+
+  public static void putBytes(final ByteBuffer buff, double val, Order order) {
+    putRaw(buff, toRawLong(val, order));
+  }
+
+  //
+  // Helper methods for interoping between float values and 4 unsigned byte
+  // sequences stored in an int.
+  //
+
+  /**
+   * Convert a double containing an application-layer value into a <i>raw
+   * long</i>, a sequence of 8 unsigned bytes. "Raw" in this context is
+   * <b>unrelated</b> to "raw" from {@link Double#doubleToRawLongBits(double)}.
+   * @param val The value to serialize.
+   * @param order the <code>Order</code> direction to respect.
+   * @return 8 unsigned bytes stored in a <code>long</code>.
+   */
+  protected static long toRawLong(double val, Order order) {
+    long raw = Double.doubleToLongBits(val);
+    return (raw ^ ((raw >> Long.SIZE - 1) | Long.MIN_VALUE));
+  }
+
+  /**
+   * Convert a <i>raw long</i> containing a sequence of 8 unsigned bytes into
+   * a double application-layer value.
+   * @param raw the 8 unsigned bytes to deserialize.
+   * @param order the <code>Order</code> direction to respect.
+   * @return an application value.
+   */
+  protected static double fromRawLong(long raw, Order order) {
+    return Double.longBitsToDouble((~raw >> Long.SIZE - 1) | Long.MIN_VALUE);
+  }
 }
--- src/main/java/types/LONG.java
+++ src/main/java/types/LONG.java
@@ -2,20 +2,18 @@ package types;
 
 import java.nio.ByteBuffer;
 
-import org.apache.hadoop.hbase.util.Bytes;
-
 import util.HSerializer;
 
 /**
- * Fixed-width Serializer for HBase LONG values.
+ * Serializer for HBase LONG values.
  * <p>
  * The HBase LONG is an 8-byte, signed, two's complement integer stored in
  * Big-Endian byte order.
  * </p>
  * <p>
- * Serialization is performed by inverting the long sign bit and writing the
- * value to a byte array in big endian order. For DESCENDING sort order, the
- * encoded value is logically inverted.
+ * Serialization is performed by inverting the <code>long</code> sign bit and
+ * writing the value to a byte array in big endian order. For DESCENDING sort
+ * order, the encoded value is logically inverted.
  * </p>
  */
 public class LONG extends HSerializer<Long> {
@@ -35,7 +33,7 @@ public class LONG extends HSerializer<Long> {
   @Override
   public Long fromBytes(byte[] bytes) {
     // TODO(perf): should we manage our own Long instance cache?
-    return Long.valueOf(toLong(bytes, 0, SIZEOF_LONG, order));
+    return Long.valueOf(toLong(bytes, 0, order));
   }
 
   //
@@ -43,24 +41,19 @@ public class LONG extends HSerializer<Long> {
   //
 
   public static long toLong(final byte[] bytes) {
-    return toLong(bytes, 0, SIZEOF_LONG, DEFAULT_ORDER);
+    return toLong(bytes, 0, DEFAULT_ORDER);
   }
 
   public static long toLong(final byte[] bytes, final Order order) {
-    return toLong(bytes, 0, SIZEOF_LONG, order);
+    return toLong(bytes, 0, order);
   }
 
   public static long toLong(final byte[] bytes, final int offset) {
-    return toLong(bytes, offset, SIZEOF_LONG, DEFAULT_ORDER);
+    return toLong(bytes, offset, DEFAULT_ORDER);
   }
 
-  public static long toLong(final byte[] bytes, final int offset, final Order order) {
-    return toLong(bytes, offset, SIZEOF_LONG, order);
-  }
-
-  public static long toLong(final byte[] bytes, final int offset, final int length, Order order) {
-    long l = Bytes.toLong(bytes, offset, length);
-    return l ^ Long.MIN_VALUE ^ order.mask;
+  public static long toLong(final byte[] bytes, final int offset, Order order) {
+    return fromRawLong(getRaw(bytes, offset), order);
   }
 
   public static byte[] toBytes(long val) {
@@ -68,8 +61,9 @@ public class LONG extends HSerializer<Long> {
   }
 
   public static byte[] toBytes(long val, Order order) {
-    val = val ^ Long.MIN_VALUE ^ order.mask;
-    return Bytes.toBytes(val);
+    byte[] buff = new byte[SIZEOF_LONG];
+    putRaw(buff, 0, toRawLong(val, order));
+    return buff;
   }
 
   public static void putBytes(final ByteBuffer buff, long val) {
@@ -77,8 +71,88 @@ public class LONG extends HSerializer<Long> {
   }
 
   public static void putBytes(final ByteBuffer buff, long val, Order order) {
-    Bytes.putLong(buff.array(), buff.arrayOffset() + buff.position(),
-      val ^ Long.MIN_VALUE ^ order.mask);
+    putRaw(buff, toRawLong(val, order));
+  }
+
+  //
+  // Helper methods for interoping between long values and 8 unsigned byte
+  // sequences stored in a long.
+  //
+
+  /**
+   * Convert a <i>value long</i> containing an application-layer value into a
+   * <i>raw long</i>, a sequence of 8 unsigned bytes.
+   * @param val The value to serialize
+   * @param order The <code>Order</code> direction to respect.
+   * @return 8 unsigned bytes stored in a <code>long</code>.
+   */
+  protected static long toRawLong(long val, Order order) {
+    return val ^ Long.MIN_VALUE ^ order.mask;
+  }
+
+  /**
+   * Convert a <i>raw long<i> containing a sequence of 8 unsigned bytes into a
+   * <i>value long</i>, an application-layer value.
+   * @param raw The 8 unsigned bytes to deserialize.
+   * @param order The <code>Order</code> direction to respect.
+   * @return an application value.
+   */
+  protected static long fromRawLong(long raw, Order order) {
+    return raw ^ Long.MIN_VALUE ^ order.mask;
+  }
+
+  //
+  // Helper methods for reading/writing 8 unsigned byte sequences from/to
+  // byte[]'s and ByteBuffer's.
+  //
+
+  /**
+   * Put a sequence of 8 unsigned bytes as <code>raw</code> into
+   * <code>buff</code>.
+   * @param buff The buffer to receive the bytes.
+   * @param raw 8 unsigned bytes.
+   */
+  protected static void putRaw(ByteBuffer buff, long raw) {
+    putRaw(buff.array(), buff.arrayOffset() + buff.position(), raw);
     buff.position(buff.position() + SIZEOF_LONG);
+  }
+
+  /**
+   * Put a sequence of 8 unsigned bytes as <code>raw</code> into <code>buff</code>.
+   * @param buff The buffer to receive the bytes.
+   * @param raw 8 unsigned bytes.
+   */
+  protected static void putRaw(byte[] buff, int offset, long raw) {
+    for (int i = offset + SIZEOF_LONG - 1; i >= offset; i--) {
+      buff[i] = (byte) raw;
+      raw >>>= 8;
+    }
+  }
+
+  /**
+   * Read a sequence of 8 unsigned bytes as a <code>raw</code> from <code>buff</code>.
+   * @param buff The buffer from which to retrieve bytes.
+   * @return 8 unsigned bytes in a <code>raw long</code>.
+   */
+  protected static long getRaw(ByteBuffer buff) {
+    long raw = getRaw(buff.array(), buff.arrayOffset() + buff.position());
+    buff.position(buff.position() + SIZEOF_LONG);
+    return raw;
+  }
+
+  /**
+   * Read a sequence of 8 unsigned bytes as a <code>raw</code> from <code>buff</code>.
+   * @param buff The buffer from which to retrieve bytes.
+   * @param offset position in buff from which to start reading.
+   * @return 8 unsigned bytes in a <code>raw long</code>.
+   */
+  protected static long getRaw(byte[] buff, int offset) {
+    assert buff.length >= offset + SIZEOF_LONG;
+    long j = 0;
+    for (int i = offset; i < offset + SIZEOF_LONG; i++) {
+      j <<= 8;
+      j ^= buff[i] & 0xFF;
+    }
+    return j;
   }
 }
