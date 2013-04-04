9ad57628eb3c39f155dca82c3b2410678107c661
Nick Dimiduk <ndimiduk@gmail.com>
2013-04-04 14:52:59 -0700

    implement VARCHAR and CHAR types

--- src/main/java/types/CHAR.java
+++ src/main/java/types/CHAR.java
@@ -1,28 +1,108 @@
 package types;
 
+import static java.lang.String.format;
 
 import java.nio.ByteBuffer;
 
 import util.HSerializer;
 
+/**
+ * Serializer for HBase CHAR values.
+ * <p>
+ * The HBase CHAR is an arbitrary number of unicode characters, limited to
+ * <code>length</code> bytes when serialized.
+ * </p>
+ * <p>
+ * Serialization is identical to that of {@link VARCHAR}, with the addition of
+ * length constraint checking.
+ * </p>
+ */
 public class CHAR extends HSerializer<String> {
+
+  private static final String TOO_LARGE_FMT = "Encoded value does not fit on %d bytes.";
+  private final int length;
+
+  public CHAR(int length) {
+    super();
+    this.length = length;
+  }
+
+  public CHAR(int length, Order order) {
+    super(order);
+    this.length = length;
+  }
+
+  /**
+   * Ensure <code>val</code> + a termination byte fit within <code>len</code>
+   * constraint. This is only a heuristic to detect overly-ambitious input
+   * Strings. Definitive checking is handled by
+   * {@link #validate(int, byte[])}.
+   * @param len maximum length of resulting encoded value.
+   * @param val String value to test.
+   * @return val, for chaining purposes.
+   */
+  protected static String validate(int len, String val) {
+    if (null == val) return val;
+    if (val.length() + 1 <= len) return val;
+    throw new IllegalArgumentException(format(TOO_LARGE_FMT, len));
+  }
+
+  /**
+   * Ensure <code>val</code> fits within <code>len</code> constraint.
+   * @param len maximum length of resulting encoded value.
+   * @param val String value to test.
+   * @param offset start validation after indexing this far into <code>val</code>.
+   * @return val, for chaining purposes.
+   */
+  protected static byte[] validate(int len, byte[] val, int offset) {
+    if (val.length - offset <= len) return val;
+    throw new IllegalArgumentException(format(TOO_LARGE_FMT, len));
+  }
 
   @Override
   public byte[] toBytes(String val) {
-    // TODO Auto-generated method stub
-    return null;
+    return validate(length, VARCHAR.toBytes(validate(length, val), order), 0);
   }
 
   @Override
   public void putBytes(ByteBuffer buff, String val) {
-    // TODO Auto-generated method stub
-
   }
 
   @Override
   public String fromBytes(byte[] bytes) {
-    // TODO Auto-generated method stub
-    return null;
+    return VARCHAR.toString(validate(length, bytes, 0), 0, order);
   }
 
+  //
+  // Helper methods to mimic {@link Bytes}
+  //
+
+  public static byte[] toBytes(int length, String val, Order order) {
+    return validate(length, VARCHAR.toBytes(validate(length, val), order), 0);
+  }
+
+  public static ByteBuffer putBytes(int length, ByteBuffer buff, String val, Order order) {
+    // TODO: reimplement with fewer allocation/copies
+    return buff.put(toBytes(length, val, order));
+  }
+
+  public static byte[] putBytes(int length, byte[] dst, int dstOffset, String val, Order order) {
+    // don't populate the ByteBuffer directly so that it remains unmodified in
+    // the event of constraint failure.
+    byte[] bytes = validate(length, VARCHAR.toBytes(validate(length, val), order), 0);
+    System.arraycopy(bytes, 0, dst, dstOffset, bytes.length);
+    return dst;
+  }
+
+  public static String toString(int length, byte[] bytes) {
+    return VARCHAR.toString(validate(length, bytes, 0), 0, DEFAULT_ORDER);
+  }
+
+  public static String toString(int length, byte[] bytes, int offset) {
+    return VARCHAR.toString(validate(length, bytes, offset), offset, DEFAULT_ORDER);
+  }
+
+  public static String toString(int length, byte[] bytes, int offset, Order order) {
+    return VARCHAR.toString(validate(length, bytes, offset), offset, order);
+  }
 }
---
+++ src/main/java/types/VARCHAR.java
@@ -0,0 +1,125 @@
+package types;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
+import util.HSerializer;
+
+/**
+ * Serializer for HBase VARCHAR values.
+ * <p>
+ * The HBase VARCHAR is an arbitrary number of unicode characters.
+ * </p>
+ * <p>
+ * Serialization is performed by writing the encoded the String as UTF-8, in
+ * big endian byte order, followed by a (null) termination byte. For
+ * DESCENDING sort order, the encoded value is logically inverted.
+ * </p>
+ */
+public class VARCHAR extends HSerializer<String> {
+
+  static final Charset UTF8 = Charset.forName("UTF-8");
+
+  static final byte NULL = (byte) 0x00;
+  static final byte TERM = (byte) 0x01;
+
+  public VARCHAR() { super(); }
+  public VARCHAR(Order order) { super(order); }
+
+  @Override
+  public byte[] toBytes(String val) {
+    return toBytes(val, order);
+  }
+
+  @Override
+  public void putBytes(ByteBuffer buff, String val) {
+    putBytes(buff.array(), buff.position(), val, order);
+  }
+
+  @Override
+  public String fromBytes(byte[] bytes) {
+    return toString(bytes);
+  }
+
+  //
+  // Helper methods to mimic {@link Bytes}
+  //
+
+  // TODO: refactor {to,put}Bytes M-w, C-y, a la LONG raw helpers.
+
+  public static byte[] toBytes(String val, Order order) {
+    if (null == val) {
+      return new byte[] { (byte) (NULL ^ order.mask), TERM };
+    }
+
+    // TODO: reimplement with fewer allocations/copies
+    byte[] encoded = val.getBytes(UTF8);
+    for (int i = 0; i < encoded.length; i++) {
+      encoded[i] = (byte) (encoded[i] + 2 ^ order.mask);
+    }
+    byte[] ret = new byte[encoded.length + 1];
+    ret[encoded.length] = TERM;
+    System.arraycopy(encoded, 0, ret, 0, encoded.length);
+    return ret;
+  }
+
+  public static byte[] putBytes(byte[] dst, int dstOffset, String val, Order order) {
+    if (null == val) {
+      assert dst.length >= dstOffset + 2;
+      dst[dstOffset] = (byte) (NULL ^ order.mask);
+      dst[dstOffset + 1] = TERM;
+      return dst;
+    }
+
+    // TODO: reimplement with fewer allocations/copies
+    byte[] encoded = val.getBytes(UTF8);
+    for (int i = 0; i < encoded.length; i++) {
+      encoded[i] = (byte) (encoded[i] + 2 ^ order.mask);
+    }
+    assert dst.length >= dstOffset + encoded.length + 1;
+    dst[dstOffset + encoded.length] = TERM;
+    System.arraycopy(encoded, 0, dst, dstOffset, encoded.length);
+    return dst;
+  }
+
+  public static ByteBuffer putBytes(ByteBuffer buff, String val, Order order) {
+    if (null == val) {
+      assert buff.limit() >= buff.position() + 2;
+      buff.put((byte) (NULL ^ order.mask));
+      return buff.put(TERM);
+    }
+
+    // TODO: reimplement with fewer allocations/copies
+    byte[] encoded = val.getBytes(UTF8);
+    for (int i = 0; i < encoded.length; i++) {
+      encoded[i] = (byte) (encoded[i] + 2 ^ order.mask);
+    }
+    assert buff.limit() >= buff.position() + encoded.length + 1;
+    System.arraycopy(encoded, 0, buff.array(), buff.position(), encoded.length);
+    buff.position(buff.position() + encoded.length);
+    return buff.put(TERM);
+  }
+
+  public static String toString(byte[] bytes) {
+    return toString(bytes, 0, DEFAULT_ORDER);
+  }
+
+  public static String toString(byte[] bytes, int offset) {
+    return toString(bytes, offset, DEFAULT_ORDER);
+  }
+
+  public static String toString(byte[] bytes, int offset, Order order) {
+    if (bytes[offset] == (byte) (NULL ^ order.mask) && bytes[offset+1] == TERM)
+      return null;
+
+    ByteBuffer decoded = ByteBuffer.allocate(bytes.length - offset);
+    for (int i = offset; i < bytes.length; i++) {
+      if (TERM == bytes[i])
+        break;
+      decoded.put((byte)(bytes[i] ^ order.mask - 2));
+    }
+    byte[] ret = new byte[decoded.position() + 1];
+    System.arraycopy(decoded.array(), 0, ret, 0, ret.length);
+    return new String(ret, UTF8);
+  }
+}
